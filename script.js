8ba7a7b6aff6198172d447ed8e21f461e5caea46
ANVI1337 <136141840+ANVI1337@users.noreply.github.com>
2023-09-21 02:40:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const btnNo = document.querySelector(".btn-no");
+
+function redirectToLink() {
+  console.log("Botão 'Não' clicado");
+  window.location.href = "https://wa.me/5582999450966"; // Substitua com o link desejado
+}
+
+function moveRandomly() {
+  console.log("Mouse passou por cima do botão 'Não'");
+  const bodyWidth = document.body.clientWidth - btnNo.clientWidth;
+  const bodyHeight = document.body.clientHeight - btnNo.clientHeight;
+
+  const randomX = Math.floor(Math.random() * bodyWidth);
+  const randomY = Math.floor(Math.random() * bodyHeight);
+
+  btnNo.style.position = "absolute";
+  btnNo.style.left = `${randomX}px`;
+  btnNo.style.top = `${randomY}px`;
+}
